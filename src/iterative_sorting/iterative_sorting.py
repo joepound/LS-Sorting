0b63eecc5f20d15155e251f6ef0ec868d268c5e2
joepound <14357797+joepound@users.noreply.github.com>
2019-09-09 12:47:31 -0600

    Added challenge files

---
+++ src/iterative_sorting/iterative_sorting.py
@@ -0,0 +1,26 @@
+# TO-DO: Complete the selection_sort() function below
+def selection_sort(arr):
+    # loop through n-1 elements
+    for i in range(0, len(arr) - 1):
+        cur_index = i
+        smallest_index = cur_index
+
+        # TO-DO: find next smallest element
+
+        # TO-DO: swap
+
+    return arr
+
+
+# TO-DO:  implement the Bubble Sort function below
+def bubble_sort(arr):
+    # TO-DO
+
+    return arr
+
+
+# STRETCH: implement the Count Sort function below
+def count_sort(arr, maximum=-1):
+    # TO-DO
+
+    return arr
---
+++ src/recursive_sorting/recursive_sorting.py
@@ -0,0 +1,34 @@
+# TO-DO: complete the helper function below to merge 2 sorted arrays
+def merge(arrA, arrB):
+    elements = len(arrA) + len(arrB)
+    merged_arr = [0] * elements
+    # TO-DO
+
+    return merged_arr
+
+
+# TO-DO: implement the Merge Sort function below USING RECURSION
+def merge_sort(arr):
+    # TO-DO
+
+    return arr
+
+
+# STRETCH: implement an in-place merge sort algorithm
+def merge_in_place(arr, start, mid, end):
+    # TO-DO
+
+    return arr
+
+
+def merge_sort_in_place(arr, l, r):
+    # TO-DO
+
+    return arr
+
+
+# STRETCH: implement the timsort function below
+def timsort(arr):
+    # TO-DO
+
+    return arr
