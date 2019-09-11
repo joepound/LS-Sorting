a5ce9de7d769eb665dc479850825f29878d810eb
joepound <14357797+joepound@users.noreply.github.com>
2019-09-11 06:31:52 -0600

    Performed cleaning of iterative sorting solutions and test cases

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,72 +1,54 @@
 # TO-DO: Complete the selection_sort() function below
-def selection_sort(arr):
-    # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
-
-        # TO-DO: find next smallest element
-        for j in range(cur_index, len(arr)):
-            moving_index = j
-            if arr[smallest_index] > arr[moving_index]:
-                smallest_index = moving_index
-
-        # TO-DO: swap
-        if smallest_index != cur_index:
-            arr[cur_index], arr[smallest_index] = (
-                arr[smallest_index], arr[cur_index]
-            )
-
-    return arr
+def selection_sort(arr, start=0, end=None):
+    if end is None:
+        end = len(arr)
+    for start in range(start, end - 1):
+        for walk in range(start + 1, end):
+            if arr[start] > arr[walk]:
+                arr[start], arr[walk] = arr[walk], arr[start]
 
 
-# TO-DO:  implement the Bubble Sort function below
-def bubble_sort(arr):
-    has_swapped = True
-    while(has_swapped):
+def bubble_sort(arr, start=0, end=None):
+    if end is None:
+        end = len(arr)
+    while True:
         has_swapped = False
-        for i in range(len(arr) - 1):
+        for i in range(start, end - 1):
             left = i
             right = i + 1
             if arr[left] > arr[right]:
-                arr[left], arr[right] = (arr[right], arr[left])
+                arr[left], arr[right] = arr[right], arr[left]
                 has_swapped = True
-    return arr
+        if not has_swapped:
+            break
 
 
 # STRETCH: implement the Count Sort function below
-def count_sort(arr, maximum=-1):
-    if len(arr) == 0:
+def count_sort(arr):
+    if len(arr) <= 1:
         return arr
-    if maximum == -1:
-        maximum = max(arr)
-    value_counts = [0] * (maximum + 1)
-    for i in arr:
-        if i < 0:
-            return "Error, negative numbers not allowed in Count Sort"
+    value_counts = [0] * (max(arr) + 1)
+    for val in arr:
+        if val < 0:
+            return "Error: count sort cannot have negative values"
         else:
-            value_counts[i] += 1
-    arr = []
-    for i in range(len(value_counts)):
-        arr += [i] * value_counts[i]
-    return arr
+            value_counts[val] += 1
+    arr.clear()
+    for value in range(len(value_counts)):
+        arr += [value] * value_counts[value]
 
 
 # ADDITIONAL: Insertion sort algorithm
-def insertion_sort(arr, start, end):
-    if end <= 1:
-        return arr
-    for i in range(start, end):
-        temp = arr[i]
-        track = None
-        for j in range(i - 1, -1, -1):
-            track = j
-            if temp < arr[j]:
-                arr[j + 1] = arr[j]
+def insertion_sort(arr, start=0, end=None):
+    if end is None:
+        end = len(arr)
+    for moving_index in range(start, end):
+        val_to_insert = arr[moving_index]
+        for insertion_index in range(moving_index - 1, start - 1, -1):
+            if val_to_insert < arr[insertion_index]:
+                arr[insertion_index + 1] = arr[insertion_index]
+                if insertion_index == 0:
+                    arr[0] = val_to_insert
             else:
-                arr[j + 1] = temp
-                track = None
+                arr[insertion_index + 1] = val_to_insert
                 break
-        if track is not None:
-            arr[track] = temp
-    return arr
--- src/iterative_sorting/test_iterative.py
+++ src/iterative_sorting/test_iterative.py
@@ -1,70 +1,55 @@
 import unittest
-import random
+from random import sample
+from copy import deepcopy
 
 
 class IterativeSortingTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.UNSORTED_TEST_LISTS = (
+            [1, 5, 8, 4, 2, 9, 6, 0, 3, 7],
+            [],
+            [9000],
+            [1, 5, 8, 8, 4, 2, 9, 2, 6, 0, 3, 7],
+            sample(range(1000), 250)
+        )
+        self.SORTED_TEST_LISTS = tuple(
+            sorted(LIST) for LIST in self.UNSORTED_TEST_LISTS
+        )
+
     def test_selection_sort(self):
         from iterative_sorting import selection_sort
 
-        arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
-        arr2 = []
-        arr3 = [0, 1, 2, 3, 4, 5]
-        arr4 = random.sample(range(200), 50)
-
-        self.assertEqual(selection_sort(arr1), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
-        self.assertEqual(selection_sort(arr2), [])
-        self.assertEqual(selection_sort(arr3), [0, 1, 2, 3, 4, 5])
-        self.assertEqual(selection_sort(arr4), sorted(arr4))
+        test_lists = deepcopy(self.UNSORTED_TEST_LISTS)
+        for i in range(len(test_lists)):
+            selection_sort(test_lists[i])
+            self.assertEqual(test_lists[i], self.SORTED_TEST_LISTS[i])
 
     def test_bubble_sort(self):
         from iterative_sorting import bubble_sort
 
-        arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
-        arr2 = []
-        arr3 = [0, 1, 2, 3, 4, 5]
-        arr4 = random.sample(range(200), 50)
-
-        self.assertEqual(bubble_sort(arr1), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
-        self.assertEqual(bubble_sort(arr2), [])
-        self.assertEqual(bubble_sort(arr3), [0, 1, 2, 3, 4, 5])
-        self.assertEqual(bubble_sort(arr4), sorted(arr4))
+        test_lists = deepcopy(self.UNSORTED_TEST_LISTS)
+        for i in range(len(test_lists)):
+            bubble_sort(test_lists[i])
+            self.assertEqual(test_lists[i], self.SORTED_TEST_LISTS[i])
 
     # Uncomment this test to test your count_sort implementation
 
-    def test_counting_sort(self):
+    def test_count_sort(self):
         from iterative_sorting import count_sort
 
-        arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
-        arr2 = []
-        arr3 = [1, 5, -2, 4, 3]
-        arr4 = random.sample(range(200), 50)
-
-        self.assertEqual(count_sort(arr1), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
-        self.assertEqual(count_sort(arr2), [])
-        self.assertEqual(
-            count_sort(arr3),
-            "Error, negative numbers not allowed in Count Sort"
-        )
-        self.assertEqual(count_sort(arr4), sorted(arr4))
+        test_lists = deepcopy(self.UNSORTED_TEST_LISTS)
+        for i in range(len(test_lists)):
+            count_sort(test_lists[i])
+            self.assertEqual(test_lists[i], self.SORTED_TEST_LISTS[i])
 
     def test_insertion_sort(self):
         from iterative_sorting import insertion_sort
 
-        arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
-        arr2 = []
-        arr3 = [0, 1, 2, 3, 4, 5]
-        arr4 = random.sample(range(200), 50)
-
-        self.assertEqual(
-            insertion_sort(arr1, 0, len(arr1)),
-            [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-        )
-        self.assertEqual(insertion_sort(arr2, 0, len(arr2)), [])
-        self.assertEqual(
-            insertion_sort(arr3, 0, len(arr3)),
-            [0, 1, 2, 3, 4, 5]
-        )
-        self.assertEqual(insertion_sort(arr4, 0, len(arr4)), sorted(arr4))
+        test_lists = deepcopy(self.UNSORTED_TEST_LISTS)
+        for i in range(len(test_lists)):
+            insertion_sort(test_lists[i])
+            self.assertEqual(test_lists[i], self.SORTED_TEST_LISTS[i])
 
 
 if __name__ == '__main__':
